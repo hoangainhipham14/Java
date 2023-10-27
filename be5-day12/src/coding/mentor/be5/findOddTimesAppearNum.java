ce0b9c94219ac3809fbc544eac581a9124993eec
nhi <hoangainhipham14@gmail.com>
2023-10-27 20:09:09 +1100

    HW Day 12
    Find square integers and numbers that appear odd times

---
+++ be5-day12/src/coding/mentor/be5/findOddTimesAppearNum.java
@@ -0,0 +1,32 @@
+package coding.mentor.be5;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class findOddTimesAppearNum {
+	
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 100, 400, 15, 15, 15, 15 };
+		Arrays.sort(arr);
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		
+		int current = arr[0];
+		int count = 0;
+		for (int num: arr) {
+			if (current == num) {
+				count += 1;
+			} else {
+				if (count % 2 != 0) {
+					res.add(current);
+				}
+				current = num;
+				count = 1;
+			}
+		}
+		
+		System.out.println("Numbers that appear odd times: ");
+		for (int num: res) {
+			System.out.print(num + " ");
+		}
+	}
+}
---
+++ be5-day12/src/coding/mentor/be5/findSquareInteger.java
@@ -0,0 +1,36 @@
+package coding.mentor.be5;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class findSquareInteger {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int numTestCases = input.nextInt();
+		int[] numSquareIntegers = new int[numTestCases];
+		
+		for (int i = 0; i < numTestCases; i++) {
+			int start = input.nextInt();
+			int end = input.nextInt();
+			ArrayList<Integer> squareIntegers = findSquareIntegers(start, end);
+			numSquareIntegers[i] = squareIntegers.size();
+		}
+		
+		for (int count: numSquareIntegers) {
+			System.out.println(count);
+		}
+		
+		input.close();
+	}
+	
+	public static ArrayList<Integer> findSquareIntegers(int start, int end) {
+		ArrayList<Integer> squareIntegers = new ArrayList<Integer>();
+		for (int i = start; i < end + 1; i++) {
+			int squareRoot = (int) Math.sqrt(i);
+			if (squareRoot == Math.sqrt(i)) {
+				squareIntegers.add(i);
+			}
+		}
+		return squareIntegers;
+	}
+}
